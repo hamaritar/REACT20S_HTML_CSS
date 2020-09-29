620208ed7caef37a0c0c895b46c2a458f2d36a9f
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-09-29 21:27:00 +0300

    sticky menu

--- 15_challenge_jobsinfinland/assets/js/main.js
+++ 15_challenge_jobsinfinland/assets/js/main.js
@@ -1,11 +1,15 @@
 const backToButton = document.getElementById("back-to-top");
 const width = window.matchMedia("(max-width: 576px)");
 const search = document.getElementById("search-form");
+const navbar = document.getElementById("main-nav");
+const main = document.getElementById("main-content");
+const sticky = navbar.offsetTop;
 
 // BACK TO TOP BUTTON
 // based on tutorial: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 window.onscroll = () => scrollCheck();
+window.onscroll = () => stickyMenu();
 
 const scrollCheck = () =>
   (document.body.scrollTop > 200 ||
@@ -22,4 +26,17 @@ const backToTop = () => {
 
 const showHide = () => {
   search.classList.toggle("hide");
+};
+
+// FIXED MENU
+// based on tutorial: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+
+const stickyMenu = () => {
+  if (window.pageYOffset >= sticky && !width.matches) {
+    navbar.classList.add("sticky");
+    main.classList.add("space");
+  } else {
+    navbar.classList.remove("sticky");
+    main.classList.remove("space");
+  }
 };
