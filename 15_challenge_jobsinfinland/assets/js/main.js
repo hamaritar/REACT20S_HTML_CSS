10b8e9028de77ee2b5b949cc59fa510970b8097f
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-09-29 23:03:43 +0300

    mobile menu

--- 15_challenge_jobsinfinland/assets/js/main.js
+++ 15_challenge_jobsinfinland/assets/js/main.js
@@ -4,6 +4,9 @@ const search = document.getElementById("search-form");
 const navbar = document.getElementById("main-nav");
 const main = document.getElementById("main-content");
 const sticky = navbar.offsetTop;
+const menu = document.getElementById("menu");
+const openM = document.getElementById("openMenu");
+const closeM = document.getElementById("closeMenu");
 
 // BACK TO TOP BUTTON
 // based on tutorial: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
@@ -40,3 +43,15 @@ const stickyMenu = () => {
     main.classList.remove("space");
   }
 };
+
+// MOBILE MENU
+
+const openMenu = ()=> {
+  closeM.classList.toggle("hide");
+  menu.style.display = 'block';
+}
+
+const closeMenu = () => {
+  closeM.classList.toggle("hide");
+  menu.style.display = 'none';
+}
