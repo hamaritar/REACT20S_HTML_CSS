8b85edfdbe07e9c4468313167b926ec0240441e0
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-10-05 14:36:00 +0300

    tasks 18 and css animation examples

---
+++ 18_mobile_menu/main.js
@@ -0,0 +1,7 @@
+/* const mobileMenu = () => {
+  const menu = document.querySelector("ul");
+  menu.classList.toggle("mobilemenu");
+}; */
+
+const mobileMenu = () =>
+  document.querySelector("ul").classList.toggle("mobilemenu");
